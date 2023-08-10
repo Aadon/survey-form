ca155ea02258db4a4467749f3e7cac2ddad9a077
Aadon <ngwujnr@gmail.com>
2023-08-10 01:27:22 +0100

    the checkbox section of the survey form

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function groceryShopping(e) {
+  e.preventDefault();
+  const pickedGroceries = document.querySelectorAll("input[name=groceries]:checked");
+
+  const pickedGroceriesArray = Array.from(pickedGroceries);
+
+  const mainDiv = document.createElement('div');
+  const ul = document.createElement('ul');
+
+  pickedGroceriesArray.forEach(function(item) {
+    const listed = document.createElement('li');
+    listed.append(item.value);
+    ul.append(listed);
+  })
+  mainDiv.append(ul);
+  document.body.append(mainDiv);
+}
+window.addEventListener("load", function () {
+  const submitBtn = document.querySelector(".btn");
+  submitBtn.addEventListener("click", groceryShopping);
+});
